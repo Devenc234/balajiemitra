3f28e0c40cb43fde554936e8150ef57c9a20f9f5
Devenc234 <devendra072@gmail.com>
2018-07-20 03:58:28 +0530

    Added base.html, list.html, static/css and static/img, Added MediaRoot to settings.py and urls.py , updated shop url and views

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -15,12 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # This is project folder urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('shop.urls', namespace='shop'))  # to add urls for shop application
+
+    # to add urls for shop application
+    # namespace to differentiate two same url name from two different apps, like shop/product , warehouse/product
+    url(r'^', include('shop.urls', namespace='shop'))
 ]
+
+if settings.DEBUG:
+    # urlpatterns+= [ url(r'^media/(?P<path>.*)',serve,{'document_root':settings.MEDIA_ROOT, }), ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
