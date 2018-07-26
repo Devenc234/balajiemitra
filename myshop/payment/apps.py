9a46ffbddb574ac692133e3caff70f79c4fdc6fe
Devenc234 <devendra072@gmail.com>
2018-07-27 03:15:59 +0530

    Added payment notification functionality but has not check it yet. Also lacked some understanding of implementation

--- myshop/payment/apps.py
+++ myshop/payment/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
-
 class PaymentConfig(AppConfig):
     name = 'payment'
+    verbose_name = 'Payment'
+
+    def ready(self):
+        import signal
+        import payment
+
+
+
---
+++ myshop/payment/signals.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404
+from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.ipn.signals import invalid_ipn_received
+from orders.models import Order
+
+# TODO: still has to check what this do and how
+def payment_notification(sender, **kwargs):
+    ipn_object = sender
+    if ipn_object.payment_status == ST_PP_COMPLETED:
+        # payment was successful
+        order = get_object_or_404(Order, id=ipn_object.invoice)
+
+        # mark the order as paid
+        order.paid = True
+        order.save()
+
+
+invalid_ipn_received.connect(payment_notification)
+
