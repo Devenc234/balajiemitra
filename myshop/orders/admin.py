331175ae5f6a4fceb11d576567957cae2b5fe5f3
Devenc234 <devendra072@gmail.com>
2018-07-29 19:07:04 +0530

    Added a custom admin view to see order details

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.http import HttpResponse
 from .models import Order, OrderItem
 from csv import writer
+from django.utils.html import format_html
+from django.urls import reverse
 
 # Register your models here.
 
 
-def export_to_csv(modeladmin,request,queryset):
+def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
@@ -31,6 +33,18 @@ def export_to_csv(modeladmin,request,queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+# This needs to above OrderItemInline class
+def order_detail(obj):
+    # return '<a href="{}">View</a>'.format(
+    #     reverse('orders:admin_order_detail', args=[obj.id]))
+    return format_html('<a href="{}">View</a>'.format(
+            reverse('orders:admin_order_detail', args=[obj.id])))
+
+
+order_detail.allow_tags = True
+
+
+# This class needs to above OrderAdmin class
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -39,7 +53,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
