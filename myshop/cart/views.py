912dc57e932e52a7b6200bdffc1faea203049dc4
Devenc234 <devendra072@gmail.com>
2018-08-03 23:33:39 +0530

    Added suggest_products in Recommender class in shop application

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
+from .recommender import Recommender
 
 # Create your views here.
 
